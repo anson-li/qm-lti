45705c501221c14b4c3019326df259cc08c5f635
Anson Li <hello@ansonli.ca>
2017-07-31 17:13:36 -0600

    update with datatables package

--- src/lti/staff.php
+++ src/lti/staff.php
@@ -57,8 +57,8 @@ require_once('model/staff.php');
 
   $script = <<< EOD
 <script src="../../../web/js/staff.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="../../../web/css/jquery.dataTables.min.css"/>
-<script type="text/javascript" src="../../../web/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../../../web/datables/css/dataTables.bootstrap.min.css"/>
+<script type="text/javascript" src="../../../web/datatables/js/dataTables.bootstrap.min.js"></script>
 <script type="text/javascript" src="../../../web/js/datatables.js"></script>
 EOD;
 
