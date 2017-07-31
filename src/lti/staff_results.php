415775a9af9ed7f0cd4b32f1d6d388a1c2e7a337
Anson Li <hello@ansonli.ca>
2017-07-31 17:50:38 -0600

    Updated datatables with better

--- src/lti/staff_results.php
+++ src/lti/staff_results.php
@@ -44,8 +44,8 @@ require_once('model/staff.php');
 
   $script = <<< EOD
 <script src="../../../web/js/staff.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="../../../web/datatables/css/jquery.dataTables.min.css"/>
-<script type="text/javascript" src="../../../web/datatables/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../../../web/css/datatables.min.css"/>
+<script type="text/javascript" src="../../../web/js/datatables.min.js"></script>
 <script type="text/javascript" src="../../../web/js/datatables.js"></script>
 EOD;
 
