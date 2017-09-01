de2175ba7f6a2ee6d88fc8d9962be7e00bc3d392
Anson Li <hello@ansonli.ca>
2017-09-01 11:17:43 -0600

    More verbose debugging.

--- src/lti/student.php
+++ src/lti/student.php
@@ -41,7 +41,7 @@ require_once('model/student.php');
   }
 
   $student->createParticipant();
-  error_log($student->participant_id);
+  error_log("participant_id: " . $student->participant_id);
   $url = $student->getAccessAssessmentNotify();
   if (isset($_SESSION['error'])) {
     $url = "error.php";
