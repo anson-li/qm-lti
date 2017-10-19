f41a371ba44f0106b97b0780b05a5c3f48288285
Anson Li <hello@ansonli.ca>
2017-10-19 11:48:08 -0600

    Add base deliveryodata services

---
+++ src/resources/BaseService.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace Questionmark\qm_services\Api;
+
+/**
+ * @file
+ * Copyright (C) 2016 Questionmark Computing Limited.
+ *
+ * License GNU GPL version 2 or later (see LICENSE.TXT file)
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+
+/**
+ * Class BaseService for making generic service calls.
+ */
+class BaseService {
+
+  public $ServiceBaseUrl = '';
+
+  public $AreaName = '';
+
+  public $ServiceUsername = '';
+
+  public $ServicePassword = '';
+
+  public $ServiceBasicAuthString = '';
+
+  public $LogTransactions = TRUE;
+
+  public $LastHttpStatus = '';
+
+  public $LastErrorCode = '';
+
+  public $LastErrorMessage = '';
+
+  /**
+   * BaseService constructor.
+   */
+  public function __construct() {
+    $this->ServiceBaseUrl = variable_get('qm_services_base_url');
+    if (!isset($this->ServiceBaseUrl) || filter_var($this->ServiceBaseUrl, FILTER_VALIDATE_URL) === FALSE) {
+      $parsedUrl = parse_url($GLOBALS['base_url']);
+      $this->ServiceBaseUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+    }
+
+    $this->AreaName = variable_get('qm_tenant_id');
+    $this->ServiceUsername = variable_get('qm_qmwise_username');
+    $this->ServicePassword = variable_get('qm_qmwise_password');
+    $this->ServiceBasicAuthString = variable_get('qm_qmwise_basicauth');
+    $this->LogTransactions = variable_get('qm_services_log_transactions', TRUE);
+  }
+
+  /**
+   * Signs Parameters using RSA Keys.
+   *
+   *   Contains any parameters needed for service call.
+   *
+   * @return array|null
+   *   Returns either null or an array containing the signed parameters
+   *   including timestamp and signature.
+   */
+  private function signParams($targetUrl, $params) {
+    $signedRequestDataGenerator = CoreFactory::getSignedRequestDataGenerator();
+
+    $params = $signedRequestDataGenerator->generateDeliveryoDataServiceParameters($targetUrl, $params);
+
+    return $params;
+  }
+
+  /**
+   * Generates cURL compatible header with timestamp and signature.
+   *
+   * @param array|null $params
+   *   Contains any parameters needed for service call including timestamp and
+   *    signature.
+   *
+   * @return object
+   *   Returns either null or an array containing the signed httpHeader for cURL
+   *   and the list of parameters in a json encoded string.
+   */
+  private function setSignedHttpHeader($params = NULL) {
+    $paramString = json_encode($params);
+    $obj = new \stdClass();
+    $obj->paramString = $paramString;
+    $obj->httpHeader = [
+      'Content-Type: application/json',
+      'x-qm-timestamp: ' . $params['timestamp'],
+      'x-qm-signature: ' . $params['signature'],
+    ];
+    return $obj;
+  }
+
+  /**
+   * Generates cURL compatible header without timestamp and signature.
+   *
+   * @param array|null $params
+   *   Contains any parameters needed for service call.
+   *
+   * @return object|null
+   *   Returns either null or an array containing the httpHeader for cURL
+   *   and the list of parameters in a json encoded string.
+   */
+  private function setHttpHeader($params = NULL) {
+    $obj = new \stdClass();
+    if (isset($params)) {
+      $paramString = json_encode($params);
+      $obj->paramString = $paramString;
+      $obj->httpHeader = [
+        'Authorization: Basic ' . $this->ServiceBasicAuthString,
+        'Content-Type: application/json',
+      ];
+    }
+    else {
+      $obj->paramString = NULL;
+      $obj->httpHeader = [
+        'Authorization: Basic ' . $this->ServiceBasicAuthString,
+        'Content-Type: application/json',
+      ];
+    }
+    return $obj;
+  }
+
+  /**
+   * Generic call to a REST based API.
+   *
+   * @param string $serviceName
+   *   String representation of the name service being called.
+   * @param string $apiMethodName
+   *   String representation of the name of the method being invoked.
+   * @param string $endpoint
+   *   String representation of the endpoint's address.
+   * @param string $method
+   *   String repsentation of the http verb GET or POST.
+   * @param array|null $params
+   *   Contains any parameters necessary for the service call.
+   *
+   * @return bool
+   *   TRUE|FALSE
+   */
+  protected function callApi($serviceName, $apiMethodName, $endpoint, $method, $params = NULL) {
+    $scriptStartTime = microtime(TRUE);
+    $options = NULL;
+    $this->LastHttpStatus = '';
+    $this->LastErrorCode = '';
+    $this->LastErrorMessage = '';
+
+    if ($ch = curl_init($endpoint)) {
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+      if ($serviceName == 'Delivery Odata Service') {
+        $signedParams = $this->signParams($endpoint, $params);
+
+        $httpHeaderObj = $this->setSignedHttpHeader($signedParams);
+      }
+      else {
+        $httpHeaderObj = $this->setHttpHeader($params);
+      }
+
+      if (isset($params)) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        $requestParams = $this->getWatchdogString($httpHeaderObj->paramString);
+      }
+      else {
+        $requestParams = "";
+      }
+
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeaderObj->httpHeader);
+
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+      $result = curl_exec($ch);
+
+      $scriptEndTime = microtime(TRUE);
+      $scriptTime = number_format(($scriptEndTime - $scriptStartTime), 2);
+
+      $info = curl_getinfo($ch);
+      $this->LastHttpStatus = $info["http_code"];
+
+      if (!curl_errno($ch)) {
+        if ($this->LogTransactions) {
+          $info = curl_getinfo($ch);
+          if ($this->isJson($result)) {
+            $responseParams = $this->getWatchdogString(json_decode($result));
+          }
+          else {
+            $responseParams = $this->getWatchdogString($result);
+          }
+
+          watchdog(
+            'QM Services',
+            '@apiMethodName (@scriptTime seconds)<br/>Endpoint: @endpoint</br>HTTP Status: @httpStatus<br/>REQUEST:<pre>@request</pre><br/>RESPONSE:<pre>@responseParams</pre><br/>Curl Info:<pre>@curl_info</pre>',
+            array(
+              '@serviceName' => $serviceName,
+              '@responseParams' => $responseParams,
+              '@requestParams' => $requestParams,
+              '@apiMethodName' => $apiMethodName,
+              '@scriptTime' => $scriptTime,
+              '@endpoint' => $endpoint,
+              '@httpStatus' => $this->LastHttpStatus,
+              '@curl_info' => print_r($info, TRUE),
+            )
+          );
+        }
+
+        if ($this->isJson($result)) {
+          $resultIsJSON = TRUE;
+          $decodedResult = json_decode($result);
+        }
+        else {
+          $resultIsJSON = FALSE;
+          $decodedResult = $result;
+        }
+
+        if ($resultIsJSON && isset($decodedResult->data)) {
+          return $decodedResult->data;
+        }
+        else {
+          if ($resultIsJSON && isset($decodedResult->value)) {
+            return $decodedResult->value;
+          }
+          else {
+            if ($resultIsJSON) {
+              return json_decode($result);
+            }
+            else {
+              return $result;
+            }
+          }
+        }
+      }
+      else {
+        $this->LastErrorCode = curl_errno($ch);
+        $this->LastErrorMessage = curl_error($ch);
+
+        $resultString = $this->getWatchdogString($result);
+
+        if (isset($result)) {
+          $status = $this->getWatchdogString($result);
+        }
+        else {
+          if (isset($result->code)) {
+            $status = $result->code . ': ' . $result->error;
+          }
+          else {
+            $status = 'Unknown error';
+          }
+        }
+
+        watchdog(
+          'QM Services',
+          '@apiMethodName (@scriptTime seconds)<br/>Endpoint: @endpoint</br>@status<br/>Curl Error: @curl_errorno - @curl_error<br/>Request Params:<pre>@request</pre>Status: <pre>@status</pre>Response:<pre>@result</pre>',
+          array(
+            '@serviceName' => $serviceName,
+            '@apiMethodName' => $apiMethodName,
+            '@scriptTime' => $scriptTime,
+            '@endpoint' => $endpoint,
+            '@status' => $status,
+            '@curl_errorno' => curl_errno($ch),
+            '@curl_error' => curl_error($ch),
+            '@request' => $requestParams,
+            '@result' => $resultString,
+          ),
+          WATCHDOG_ERROR);
+        return FALSE;
+      }
+    }
+    else {
+      // Curl init failed.
+      watchdog('QM Services',
+        '@apiMethodName<br/>Endpoint: @endpoint</br>API call initialization failed.<br/>Request Params<pre>@request</pre>',
+        array(
+          '@serviceName' => $serviceName,
+          '@endpoint' => $endpoint,
+          '@apiMethodName' => $apiMethodName,
+          '@request' => $this->getWatchdogString($params),
+        ),
+        WATCHDOG_ERROR);
+    }
+    curl_close($ch);
+
+    return FALSE;
+  }
+
+  /**
+   * Turns objects and arrays into a string ready for use in a watchdog call.
+   *
+   * @param array|object $obj
+   *   A stdClass that will be made ready for being logged in watchdog.
+   *
+   * @return string
+   *   String ready to be used in a call for Watchdog.
+   */
+  protected function getWatchdogString($obj) {
+    if (is_object($obj) || is_array($obj)) {
+      $messageObj = print_r($obj, TRUE);
+    }
+    else {
+      if (!is_null($obj)) {
+        $messageObj = $obj;
+      }
+      else {
+        $messageObj = 'NULL';
+      }
+    }
+
+    return $messageObj;
+  }
+
+  /**
+   * Determines if a string is already in JSON format or not.
+   *
+   * @param string $string
+   *   The string you would like to check for JSON format.
+   *
+   * @return bool
+   *   TRUE|FALSE
+   */
+  protected function isJson($string) {
+    return is_string($string) && is_array(json_decode($string, TRUE)) && (json_last_error() == JSON_ERROR_NONE) ? TRUE : FALSE;
+  }
+
+}
---
+++ src/resources/DeliveryOdataService.php
@@ -0,0 +1,398 @@
+<?PHP
+/**
+ * Copyright (C) 2016 Questionmark Computing Limited.
+ * License GNU GPL version 2 or later (see LICENSE.TXT file)
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+
+/**
+ * Class DeliveryOdataService
+ *
+ * @package Questionmark\qm_services\Api
+ */
+class DeliveryOdataService extends BaseService {
+
+  private $ServiceEndpoint = '';
+  private $ServiceName = 'Delivery Odata Service';
+
+  public function __construct() {
+    parent::__construct();
+    $this->ServiceEndpoint = $this->ServiceBaseUrl . '/deliveryodata/' . $this->AreaName;
+  }
+
+  // RESULTS FEED
+  function GetResult($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Results";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetResults($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Results?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // GROUPS FEED
+  function GetGroup($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Groups";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetGroups($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Groups?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Rubrics FEED
+  function GetRubric($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Rubrics";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetRubrics($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Rubrics?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Dimensions FEED
+  function GetDimension($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Dimensions";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetDimensions($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Dimensions?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Questions FEED
+  function GetQuestion($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Questions";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetQuestions($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Questions?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // QuestionTranslations FEED
+  function GetQuestionTranslation($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/QuestionTranslations";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetQuestionTranslations($filter) {
+    $endpoint = $this->ServiceEndpoint . "/QuestionTranslations?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Participants FEED
+  function GetParticipant($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Participants";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetParticipants($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Participants?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Assessments FEED
+  function GetAssessment($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Assessments";
+    if (isset($id)) {
+      $endpoint .= "('" . $id . "')";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAssessments($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Assessments?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAllAssessments() {
+    $endpoint = $this->ServiceEndpoint . "/Assessments";
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Answers FEED
+  function GetAnswer($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Answers";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAnswers($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Answers?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // ScoringTasks FEED
+  function GetScoringTask($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/ScoringTasks";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetScoringTasks($filter) {
+    $endpoint = $this->ServiceEndpoint . "/ScoringTasks?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // ScoringResults FEED
+  function GetScoringResult($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/ScoringResults";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetScoringResults($filter) {
+    $endpoint = $this->ServiceEndpoint . "/ScoringResults?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // DimensionScores FEED
+  function GetDimensionScore($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/DimensionScores";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetDimensionScores($filter) {
+    $endpoint = $this->ServiceEndpoint . "/DimensionScores?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Administrators FEED
+  function GetAdministrator($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Administrators";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAdministrators($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Administrators?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // AssessmentSnapshots FEED
+  function GetAssessmentSnapshot($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/AssessmentSnapshots";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAssessmentSnapshots($filter) {
+    $endpoint = $this->ServiceEndpoint . "/AssessmentSnapshots?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // AssessmentSnapshotsData FEED
+  function GetAssessmentSnapshotsData($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/AssessmentSnapshots";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAssessmentSnapshotsDatas($filter) {
+    $endpoint = $this->ServiceEndpoint . "/AssessmentSnapshots?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // Attempts FEED
+  function GetAttempt($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/Attempts";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAttempts($filter) {
+    $endpoint = $this->ServiceEndpoint . "/Attempts?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  /**
+   * CreateAttemptsExamLobby
+   *
+   * $params = new \StdClass;
+   * $params->ExternalAttemptID = $bvAppointmentID;
+   * $params->AssessmentID = $assessmentID;
+   * $params->ParticipantID = $user->field_qms_user_participant_id['und'][0]['value'];
+   * $params->LockStatus = true;
+   * $params->LockRequired = true;
+   * $result = $deliveryAPI->CreateExamLobby($params);
+   * @return null
+   */
+  function CreateExamLobby($params) {
+    $endpoint = $this->ServiceEndpoint . "/Attempts";
+    $method = "POST";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method, $params);
+  }
+
+  function GetExamLobby($attemptID = null) {
+    $endpoint = $this->ServiceEndpoint . "/Attempts";
+    if (isset($attemptID)) {
+      $endpoint .= "(" . $attemptID . ")";
+    }
+    $method = "GET";
+      return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function SetProctorSystemWidgetURL($lobbyID, $params) {
+    $endpoint = $this->ServiceEndpoint . "/Attempts(" . $lobbyID . ")";
+    $method = "PATCH";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method, $params);
+  }
+
+  /**
+   * Get the attempt list object by ID.
+   *
+   * @param int $id
+   *    The ID of the attempt list
+   *
+   * @return object
+   *    The attempt list object
+   */
+  function GetAttemptList($id) {
+    $endpoint = $this->ServiceEndpoint . "/AttemptLists(" . $id . ")";
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  /**
+   * Gets a list of attempt list objects using the provided filter.
+   *
+   * @param string $filter
+   *    Properly formatted OData filter string, excluding '?$filter='.
+   * @param bool $expandAttempts
+   *
+   * @return array
+   *    An array of attempt list objects matching the filter
+   */
+  function GetAttemptLists($filter, $expandAttempts = FALSE) {
+    $endpoint = $this->ServiceEndpoint . "/AttemptLists?\$filter=".urlencode($filter);
+    if ($expandAttempts) $endpoint .= '&$expand=Attempts';
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  /**
+   * Creates an attempt list.
+   *
+   * @param StdClass
+   *    ExternalAttemptListID = external attempt ID (i.e. exam event ID)
+   *
+   * @return object
+   *    Newly created attempt list object
+   */
+  function CreateAttemptList($params) {
+    $endpoint = $this->ServiceEndpoint . "/AttemptLists";
+    $method = "POST";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method, $params);
+  }
+
+  // AnswerUploads FEED
+  function GetAnswerUpload($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/AnswerUploads";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetAnswerUploads($filter) {
+    $endpoint = $this->ServiceEndpoint . "/AnswerUploads?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  // PrintBatches FEED
+  function GetPrintBatch($id = null) {
+    $endpoint = $this->ServiceEndpoint . "/PrintBatches";
+    if (isset($id)) {
+      $endpoint .= "(" . $id . ")";
+    }
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+  function GetPrintBatches($filter) {
+    $endpoint = $this->ServiceEndpoint . "/PrintBatches?\$filter=".urlencode($filter);
+    $method = "GET";
+    return $this->callApi($this->ServiceName, __FUNCTION__, $endpoint, $method);
+  }
+
+}
+
