839e602459e3d9559ed67f9665b83a301f3249eb
Anson Li <hello@ansonli.ca>
2017-10-24 15:49:19 -0600

    date is its own function

--- src/resources/delivery_odata/BaseService.php
+++ src/resources/delivery_odata/BaseService.php
@@ -58,7 +58,7 @@ class BaseService {
     if (isset($params)) {
       // Create and add signature.
       $parameters = array(
-        'timestamp' => $this->date('Y-m-d H:i:sO'),
+        'timestamp' => date('Y-m-d H:i:sO'),
         'requesturl' => $requestUrl,
         'requestbody' => $params,
       );
@@ -66,7 +66,7 @@ class BaseService {
     else {
       // Create and add signature.
       $parameters = array(
-        'timestamp' => $this->date('Y-m-d H:i:sO'),
+        'timestamp' => date('Y-m-d H:i:sO'),
         'requesturl' => $requestUrl,
         'requestbody' => '',
       );
