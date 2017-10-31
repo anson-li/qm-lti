f304ec4007cec8604fda1d8a80e6c6b16a195b1c
Anson Li <hello@ansonli.ca>
2017-10-31 12:47:49 -0600

    fixed attr getter

--- src/resources/lib.php
+++ src/resources/lib.php
@@ -844,7 +844,7 @@ function is_best_result($db, $consumer, $resource_link, $user_id, $score) {
 function get_latest_attempt($db, $consumer_key, $resource_link_id, $assessment_id, $user_id) {
   $data_connector = LTI_Data_Connector::getDataConnector(TABLE_PREFIX, $db, DATA_CONNECTOR);
   $latest_attempt = $data_connector->Attempts_getLatestAttempt($consumer_key, $resource_link_id, $assessment_id, $user_id);
-  if (!latest_attempt) {
+  if (!$latest_attempt) {
     $latest_attempt = $data_connector->Attempts_setLatestAttempt($consumer_key, $resource_link_id, $assessment_id, $user_id);
   }
   return $latest_attempt;
