0273a15f8d632867dd463853c7edd33b05431764
Anson Li <hello@ansonli.ca>
2017-11-07 17:20:45 -0700

    fixed delete sql

--- src/resources/LTI_Data_Connector_qmp.php
+++ src/resources/LTI_Data_Connector_qmp.php
@@ -764,8 +764,6 @@ class LTI_Data_Connector_QMP extends LTI_Data_Connector {
     $query->bindValue('assessment', $assessment_id, PDO::PARAM_STR);
     $query->bindValue('participant', $participant_id, PDO::PARAM_STR);
     $ok = $query->execute();
-    error_log(print_r($query, 1));
-    error_log(print_r($query->errorInfo(), 1));
     if ($ok) {
       $row = $query->fetch();
       $schedule_id = $row['schedule_id'];
@@ -798,14 +796,16 @@ class LTI_Data_Connector_QMP extends LTI_Data_Connector {
  */
   public function Attempts_deleteLatestAttempt($consumer_key, $resource_link_id, $assessment_id, $schedule_id, $participant_id) {
     $sql = 'DELETE FROM ' . $this->dbTableNamePrefix . LTI_Data_Connector::ATTEMPTS_TABLE_NAME . ' ' .
-           'WHERE (consumer_key = :consumer) AND (context_id = :context) AND (assessment_id = :assessment) AND (schedule_id = :schedule) AND (customer_id = :customer)';
+           'WHERE (consumer_key = :consumer) AND (context_id = :context) AND (assessment_id = :assessment) AND (schedule_id = :schedule) AND (participant_id = :participant)';
     $query = $this->db->prepare($sql);
     $query->bindValue('consumer', $consumer_key, PDO::PARAM_STR);
     $query->bindValue('context', $resource_link_id, PDO::PARAM_STR);
     $query->bindValue('assessment', $assessment_id, PDO::PARAM_STR);
     $query->bindValue('schedule', $schedule_id, PDO::PARAM_STR);
-    $query->bindValue('participant_id', $participant_id, PDO::PARAM_STR);
+    $query->bindValue('participant', $participant_id, PDO::PARAM_STR);
     $ok = $query->execute();
+    error_log(print_r($query, 1));
+    error_log(print_r($query->errorInfo(), 1));
     return $ok;
   }
 
