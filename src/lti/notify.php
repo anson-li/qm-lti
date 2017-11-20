db17ffed2684725c73b13465a0b9f45523b79e1c
Anson Li <hello@ansonli.ca>
2017-11-20 11:46:58 -0700

    testing outcomes checking

--- src/lti/notify.php
+++ src/lti/notify.php
@@ -86,7 +86,9 @@ require_once('../resources/LTI_Data_Connector_qmp.php');
     if ($resource_link->hasOutcomesService()) {
       // Save result
       if ($resource_link->doOutcomesService(LTI_Resource_Link::EXT_WRITE, $outcome)) {
-        if ($outcome->getAttempt($consumer, $resource_link, $schedule_id, $participant_id) != FALSE) {
+        error_log("Testing outcomes checking");
+        error_log(print_r($outcome->getAttempt($consumer, $resource_link, $schedule_id, $participant_id), 1));
+        if ($outcome->getAttempt($consumer, $resource_link, $schedule_id, $participant_id) !== FALSE) {
           $outcome->clearAccessedResult($consumer, $resource_link, $participant_id);
           $outcome->saveToResult($consumer, $resource_link, $participant_id, 1, $result_id);
           $outcome->deleteAttempt($consumer, $resource_link, $schedule_id, $participant_id);
@@ -100,7 +102,7 @@ require_once('../resources/LTI_Data_Connector_qmp.php');
     }
   } else {
     $resource_link->checkValueType($outcome);
-    if ($outcome->getAttempt($consumer, $resource_link, $schedule_id, $participant_id) != FALSE) {
+    if ($outcome->getAttempt($consumer, $resource_link, $schedule_id, $participant_id) !== FALSE) {
       $outcome->saveToResult($consumer, $resource_link, $participant_id, 0, $result_id);
       $outcome->deleteAttempt($consumer, $resource_link, $participant_id, $schedule_id, $participant_id);
       delete_schedule($schedule_id);
