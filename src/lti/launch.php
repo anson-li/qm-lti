fa70ddaab51ef0f4c7d553df626e14a646b5845e
Anson Li <hello@ansonli.ca>
2018-02-05 14:18:37 -0700

    Added stub for additional_params

--- src/lti/launch.php
+++ src/lti/launch.php
@@ -113,6 +113,8 @@ require_once('../resources/LTI_Data_Connector_qmp.php');
       $return_url = parse_url($_SESSION['lti_return_url']);
       if (!empty($return_url)) {
         parse_str($return_url['query'], $_SESSION['additional_params']);
+      } else {
+        $_SESSION['additional_params'] = array();
       }
 
       // set redirect URL
