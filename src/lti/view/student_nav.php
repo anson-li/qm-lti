c1fc5384a6ed2150e6b3831cd2bdb08cd0d460f6
Anson Li <hello@ansonli.ca>
2018-02-15 08:32:15 -0700

    Removed default styling and added a line break

--- src/lti/view/student_nav.php
+++ src/lti/view/student_nav.php
@@ -1,11 +1,12 @@
 <div id="MainContentWrapper" class="col-md-6 col-md-offset-3" style="padding-top: 5em">
   <div id="ContentWrapper">
     <div id="HeaderWrapper" class="header-top padding-top-md">
-      <img id="logoImage" src="/web/images/logo.gif" alt="Questionmark" class="center-block" style="width: 175px; height: 32px; margin-left: 10px" />
+      <img id="logoImage" src="/web/images/logo.gif" alt="Questionmark" class="center-block" />
     </div>
+    <hr class="qm-divider" />
     <div id="PageContent">
       <div class="container-fluid"><p><button type="button" class="btn btn-default" onclick="location.href=\'' . $_SESSION['lti_return_url'] . '\';">Return to course environment</button></p></div>
-        <div id="body" class="container-fluid col-md-6 offset-md-3">
+        <div id="body" class="container-fluid">
         <h1>Student Portal</h1>
         <form action="student_nav.php" method="POST">
           <table class="DataTable table table-sm" cellpadding="0" cellspacing="0">
