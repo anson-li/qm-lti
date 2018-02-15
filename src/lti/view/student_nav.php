1e187ed023a7dd54b17bf6dca694b8c5081a36c8
Anson Li <hello@ansonli.ca>
2018-02-15 14:46:15 -0700

    Added echo return URL

--- src/lti/view/student_nav.php
+++ src/lti/view/student_nav.php
@@ -7,7 +7,7 @@
         </div>
         <hr class="qm-divider" />
         <div id="PageContent" class="block-color">
-          <div class="container-fluid"><p><button type="button" class="btn btn-default" style="padding: 15px 35px; background-color: #eee; border-color: #fff; color: #333;" onclick="location.href='<?php $_SESSION['lti_return_url'] ?>'">Return to course environment</button></p></div>
+          <div class="container-fluid"><p><button type="button" class="btn btn-default" style="padding: 15px 35px; background-color: #eee; border-color: #fff; color: #333;" onclick="location.href='<?php echo $return_url; ?>'">Return to course environment</button></p></div>
             <div id="body" class="container-fluid" style="padding: 0px">
             <form action="student_nav.php" method="POST">
               <div class="col-md-12">
