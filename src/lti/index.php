d4617da294c0ed5cd6545f18f51e77fd33eb1f5c
Anson Li <hello@ansonli.ca>
2018-03-28 09:12:06 -0600

    Add further tooltip

--- src/lti/index.php
+++ src/lti/index.php
@@ -116,7 +116,9 @@ require_once('../resources/lib.php');
 
   $script = <<< EOD
 <script type="text/javascript">
-  $('#rowchecksum').tooltip();
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 </script>
 EOD;
 
@@ -177,7 +179,7 @@ EOD;
           <div class="col1">
             <div id='rowchecksum' data-toggle="tooltip" title="Some learning management systems use an older version of the LTI that requires the checksum to be reduced. Enabling this feature could lower the security of your LTI connector.">
               Use reduced checksum
-            </a>
+            </div>
           </div>
           <div class="col2">
             <input type="checkbox" id="reduced_checksum" name="reduced_checksum" value="1" />
